388387dcf0bed4f0828673420ca14d20cf6ae8f6
Mark Vincze <mrk.vincze@gmail.com>
2016-05-12 17:41:53 +0200

    Update appixinstall.ps1

--- appixinstall.ps1
+++ appixinstall.ps1
@@ -10,22 +10,21 @@ param (
 }
 
 Function Broadcast-WMSettingsChange { 
-    if (-not ("win32.nativemethods" -as [type])) {
-        Add-Type -Namespace Win32 -Name NativeMethods -MemberDefinition @"
+  if (-not ("win32.nativemethods" -as [type])) {
+    Add-Type -Namespace Win32 -Name NativeMethods -MemberDefinition @"
 [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
 public static extern IntPtr SendMessageTimeout(
    IntPtr hWnd, uint Msg, UIntPtr wParam, string lParam,
    uint fuFlags, uint uTimeout, out UIntPtr lpdwResult);
 "@
-    }
+  }
  
-    $HWND_BROADCAST = [intptr]0xffff;
-    $WM_SETTINGCHANGE = 0x1a;
-    $result = [uintptr]::zero
+  $HWND_BROADCAST = [intptr]0xffff;
+  $WM_SETTINGCHANGE = 0x1a;
+  $result = [uintptr]::zero
  
     # notify all windows of environment block change
-    [win32.nativemethods]::SendMessageTimeout($HWND_BROADCAST, $WM_SETTINGCHANGE,
-            [uintptr]::Zero, "Environment", 2, 5000, [ref]$result) >$null 2>&1;
+  [win32.nativemethods]::SendMessageTimeout($HWND_BROADCAST, $WM_SETTINGCHANGE, [uintptr]::Zero, "Environment", 2, 5000, [ref]$result) >$null 2>&1;
 }
 
 Function AddTo-SystemPath {
